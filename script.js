de2813dfe93cfdfe16a81e97f2d84299b084f318
Eva Alonso Contreras <Eva.Alonso@proton.me>
2024-10-29 09:39:08 +0100

    finished the exercise

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-const jokeEl = document.('')
-const jokeBtn = document.('')
+const jokeEl = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokeBtn');
 
-jokeBtn.addEventListener('', generateJoke)
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
 
 // USING ASYNC/AWAIT
 async function generateJoke() {
   const config = {
     headers: {
-      Accept: '',
+      Accept: 'application/json',
     },
   }
 
-  const res = await ('', config)
+  const res = await fetch('https://icanhazdadjoke.com', config);
 
-  const data = await res.json()
+  const data = await res.json();
 
-  jokeEl.innerHTML = data.joke
+  jokeEl.innerHTML = data.joke;
 }
 
 //   HINTS
